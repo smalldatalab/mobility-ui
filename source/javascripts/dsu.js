a7a437c8079f26b9f23a90b497df8e255d681ace
Judy Wu <judyzhaoxinwu@gmail.com>
2015-06-20 19:13:37 -0400

    Editing variables

---
+++ source/javascripts/dsu.js
@@ -0,0 +1,10 @@
+var dsu = "https://ohmage-omh.smalldata.io/dsu/";
+
+function getDatapointId(username, date, device){
+    return  ["mobility-daily-segments", username, date, device].join("-");
+}
+
+function getSummaryDatapointId(username, date, device){
+    return  ["mobility-daily-summary", username, date, device.toLowerCase()].join("-");
+}
+
